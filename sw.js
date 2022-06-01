0d55c8a9a15fb7bfc9df411573702d2efec55a05
lazy-guy <devmadhursaini@gmail.com>
2022-06-01 12:44:28 +0530

    Updated service worker

--- sw.js
+++ sw.js
@@ -1,27 +1,30 @@
-const cacheName = 'lazy-reversi';
+const cacheName = "lazy-reversi";
 const contentToCache = [
-  './index.html',
-  './index.css',
-  './index.js',
-  './sw.js',
-  './icon.png',
-  './GitHub-Mark-64px.png',
-  './manifest.json'
+	"./index.html",
+	"./index.css",
+	"./index.js",
+	"./sw.js",
+	"./icon.png",
+	"./GitHub-Mark-64px.png",
+	"./manifest.json",
+	"./icon512.png",
+	"./favicon.png",
+	"./apple-icon.png",
 ];
 
 self.addEventListener("install", (e) => {
 	console.log("Service Worker installed");
-	e.waitUntil((async () => {
-		const cache = await caches.open(cacheName);
-		console.log('[Service Worker] Caching all: app shell and content');
-		await cache.addAll(contentToCache);
-	  })());
+	e.waitUntil(
+		(async () => {
+			const cache = await caches.open(cacheName);
+			await cache.addAll(contentToCache);
+		})()
+	);
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request)
-        })
-    )
-})
+self.addEventListener("fetch", function (event) {
+	event.respondWith(fetch(event.request).catch(() => {
+		return caches.match(event.request)
+	})
+	);
+});
